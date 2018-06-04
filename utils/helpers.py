406bf646dd5e98cb79257be5365a2076c28dcf1c
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-06-04 15:55:51 +0200

    Subclass aeti for name convention

--- utils/helpers.py
+++ utils/helpers.py
@@ -63,6 +63,69 @@ _" + self._input_temporal_resolution()
         if self.t_resolution == TIME_RESOLUTION.short_annual.value:
             return TIME_RESOLUTION.short_dekadal.value
 
+
+class AETIName(Name):
+    """ Manage AETI name convention on GEE.
+
+        Example dataset: ETI 
+        
+        input:
+            {L1_E_D,L1_T_D,L1_I_D}
+            year: 2017
+            level: L1
+            component: E,T,I
+            temporal_resolution: D
+        
+        output:
+            {L1_AETI_D/L1_AETI_1706}
+            level: L1
+            component: ETI
+            temporal_resolution: D
+    """
+
+    def src_collection(self):
+        res = []
+        eti = self.component.strip("A")
+        for scomp in tuple(eti):
+            res.append(self.level + "_" + scomp + "\
+_" + self._input_temporal_resolution())
+        return res
+
+    def _input_temporal_resolution(self):
+        # E,T,I, Dekadal to AETI Dekadal
+        if self.t_resolution == TIME_RESOLUTION.short_dekadal.value:
+            return TIME_RESOLUTION.short_dekadal.value
+
+    def dst_images(self):
+        imgs = []
+        if self._input_temporal_resolution(
+    ) == TIME_RESOLUTION.short_dekadal.value:
+            for dekad in range(1, 37):
+                imgs.append(
+                    self.dst_image() + str(dekad)
+                )
+            return imgs
+
+    def dst_asset_ids(self):
+        asset_ids = []
+        if self._input_temporal_resolution(
+    ) == TIME_RESOLUTION.short_dekadal.value:
+            for dst_image in self.dst_images():
+                asset_ids.append(
+                        os.path.join(
+                        os.path.join(
+                            self.ee_container,
+                            self.level
+                        ),
+                        os.path.join(
+                            self.dst_collection(),
+                            dst_image
+                        )
+                    )
+                )
+            return asset_ids
+
+
 class TIME_RESOLUTION(Enum):
     dekadal = "DEKADAL"
     short_dekadal = "D"
